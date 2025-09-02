391e877eea7d1db4882223651d0cb0e731f417da
Jozef Steinhübl <contact@xhyrom.dev>
2025-09-02 21:21:11 +0200

    feat(broker): use 25576

--- broker/src/index.ts
+++ broker/src/index.ts
@@ -2,12 +2,12 @@ const relays = {
   "ch-zh-1": {
     id: "ch-zh-1",
     host: "ch-zh1.retrolime.e4mc.xhyrom.dev",
-    port: 25575,
+    port: 25576,
   },
   "ch-zh-2": {
     id: "ch-zh-2",
     host: "ch-zh2.retrolime.e4mc.xhyrom.dev",
-    port: 25575,
+    port: 25576,
   },
 } as const;
 
